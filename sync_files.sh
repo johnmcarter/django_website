eb5063d3b909a8d7a0587da2ac1898582117edce
johnmcarter <jmcarter211@gmail.com>
2021-08-10 21:18:33 -0400

    edited gitignore

--- sync_files.sh
+++ sync_files.sh
@@ -1,6 +1,6 @@
 # Author: John Carter
 # Created: 2021/07/04 17:57:02
-# Last modified: 2021/08/10 21:14:17
+# Last modified: 2021/08/10 21:16:50
 
 GRN=$'\e[1;32m'
 END=$'\e[0m'
@@ -8,6 +8,7 @@ END=$'\e[0m'
 echo "[${GRN}INFO${END}] Pulling new code from git"
 cd django_website
 git pull
+
 echo "[${GRN}INFO${END}] Copying data..."
 cd ..
 cp -r django_website/ /home/john/
