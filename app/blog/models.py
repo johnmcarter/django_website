9816f41815edebadf9c665cc90d089313adca50b
John Carter <jmcarter211@gmail.com>
2024-06-24 21:42:21 -0400

    made rating for blog posts

--- app/blog/models.py
+++ app/blog/models.py
@@ -4,6 +4,7 @@ Created: 2021/08/07 21:12:00
 Last modified: 2023/06/25 18:25:13
 """
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class BlogPost(models.Model):
@@ -12,4 +13,10 @@ class BlogPost(models.Model):
     pub_date = models.DateTimeField("date published")
     location = models.CharField(max_length=200)
     website = models.URLField()
+    rating = models.IntegerField(
+        validators=[
+            MinValueValidator(0, message='Rating must be at least 0.'),
+            MaxValueValidator(5, message='Rating must be at most 5.')
+        ]
+    )
     image = models.ImageField(upload_to="blog_images/")
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,7 +1,8 @@
 '''
 Copyright 2024 by John Carter
 '''
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from django.core.paginator import Paginator
 
 from .models import BlogPost
@@ -26,9 +27,23 @@ def jjp_blog(request):
 
 def blog_post(request, post_id):
     '''Display one blog post'''
-    post = BlogPost.objects.get(id=post_id)
+    post = get_object_or_404(BlogPost, id=post_id)
+
+    # Get next and previous posts based on reverse pub_date ordering
+    next_post = BlogPost.objects.filter(
+        Q(pub_date__lt=post.pub_date) | 
+        Q(pub_date=post.pub_date, id__lt=post_id)
+    ).order_by('-pub_date', '-id').first()
+
+    previous_post = BlogPost.objects.filter(
+        Q(pub_date__gt=post.pub_date) | 
+        Q(pub_date=post.pub_date, id__gt=post_id)
+    ).order_by('pub_date', 'id').first()
+
     context = {
         'photo_url': PHOTO_URL,
-        'post': post
+        'post': post,
+        'next_post': next_post,
+        'previous_post': previous_post
     }
     return render(request, 'blog_post.html', context)
